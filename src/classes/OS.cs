27698f6d3759a90dbefa6bd24a62b8846efe2325
echokrist <echokrist@pm.me>
2025-01-12 19:22:28 +0100

    fix: Issues with program_pre_check and create_application_data_folder. As well as OS recognizations.

--- src/classes/OS.cs
+++ src/classes/OS.cs
@@ -7,292 +7,231 @@ namespace DCEUI.classes;
 public class OS : IOS
 {
     public string application_name = "DCrane";
-
     private string application_data_folder_path = "";
-
     private string os_cli_response = "";
-
     private Docker? docker = null;
 
-    public static bool Is_windows() => RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
-
-    public static bool Is_macos() => RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.OSX);
-
-    public static bool Is_gnu_linux() => RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux);
-
+    public static bool Is_windows() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    public static bool Is_macos() => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+    public static bool Is_gnu_linux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
     public OS()
     {
         try
         {
-            this.program_pre_check();
-            this.create_application_data_folder();
+            program_pre_check();
+            create_application_data_folder();
         }
-        catch (Exception)
+        catch (Exception e)
         {
-            throw new Exception("Could not create application data folder.");
+            throw e;
         }
     }
 
-    public string get_application_data_folder()
-    {
-        return this.application_data_folder_path;
-    }
+    public string get_application_data_folder() => this.application_data_folder_path;
 
-    public Docker? get_Docker_instance()
-    {
-        return this.docker ?? null;
-    }
+    public Docker? get_Docker_instance() => this.docker;
 
-    public void set_Docker_instance(Docker docker)
-    {
-        this.docker = docker;
-    }
+    public void set_Docker_instance(Docker docker) => this.docker = docker;
 
-    public string get_os_cli_response()
-    {
-        return this.os_cli_response;
-    }
+    public string get_os_cli_response() => this.os_cli_response;
 
     private void create_application_data_folder()
     {
         try
         {
-            var result = Directory.CreateDirectory(application_data_folder_path);
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-    }
+            application_data_folder_path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                application_name
+            );
 
-    public void program_pre_check()
-    {
-        try
-        {
-            if (!is_supported_platform())
-            {
-                throw new SystemException("The application does not support your Operating System.");
-            }
-
-            if (!is_docker_installed())
-            {
-                throw new SystemException("Docker CLI needs to be installed.");
-            }
-
-            if (!is_docker_currently_running_as_service())
-            {
-                throw new SystemException("Docker CLI needs to be running as a service.");
-            }
-
+            Directory.CreateDirectory(application_data_folder_path);
         }
         catch (Exception ex)
         {
-            throw;
+            throw new Exception($"Failed to create application data folder at '{application_data_folder_path}': {ex.Message}");
+        }
+    }
+
+
+    public void program_pre_check()
+    {
+        if (!is_supported_platform())
+        {
+            throw new SystemException("The application does not support your Operating System.");
+        }
+
+        if (!is_docker_installed())
+        {
+            throw new SystemException("Docker CLI needs to be installed.");
+        }
+
+        if (!is_docker_currently_running_as_service())
+        {
+            throw new SystemException("Docker CLI needs to be running as a service.");
         }
     }
 
     public bool is_supported_platform()
     {
-        if (Is_windows())
+        if (Is_windows() || Is_macos() || Is_gnu_linux())
         {
-            application_data_folder_path = Directory.GetCurrentDirectory() + "\\data";
             return true;
         }
-        else if (Is_macos())
-        {
-            application_data_folder_path = Directory.GetCurrentDirectory() + "/data";
-            return true;
-        }
-        else if (Is_gnu_linux())
-        {
-            application_data_folder_path = Directory.GetCurrentDirectory() + "/data";
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 
     public bool is_docker_installed()
     {
         string response = this.run_command("docker", "-v");
-        if (response.Contains("error") || response.Contains("Command not found"))
-        {
-            return false;
-        }
-
-        return true;
+        return !string.IsNullOrEmpty(response) && !response.ToLower().Contains("error");
     }
 
     public bool is_docker_currently_running_as_service()
     {
-        var processes = Process.GetProcessesByName("Docker").ToArray();
-
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-        if (processes.Length > 0 && processes.GetValue(0).ToString().Contains("docker"))
+        try
         {
-            return true;
-        }
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            // Attempt to run a simple Docker CLI command
+            var result = run_command("docker", "info");;
 
-        return false;
+            // If the command succeeds, Docker is running
+            return !string.IsNullOrEmpty(result) && !result.ToLower().Contains("error");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Docker service check failed: {ex.Message}");
+            return false;
+        }
     }
 
     public string get_all_files_in_application_backup_folder(string file_extention = "")
     {
-        string folderFiles = "";
-
-        foreach (string file in Directory.EnumerateFiles(
-            this.application_data_folder_path,
-            "*",
-            SearchOption.AllDirectories)
-            )
+        if (string.IsNullOrEmpty(application_data_folder_path))
         {
-            if (file is not null && file.Contains(file_extention) && file.EndsWith(".tar"))
-            {
-                folderFiles += file.Substring(application_data_folder_path.Length + 1) + "\n";
-            }
+            throw new Exception("Application data folder path is not set.");
         }
 
-        return folderFiles;
+        return string.Join("\n", Directory.EnumerateFiles(
+            application_data_folder_path,
+            "*" + file_extention + "*.tar",
+            SearchOption.AllDirectories
+        ));
     }
 
     public string run_command(string command, string args)
     {
-        var process = new Process()
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = command,
-                Arguments = args,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-            }
-        };
-        process.Start();
-
-        string output = process.StandardOutput.ReadToEnd();
-        string error = process.StandardError.ReadToEnd();
-        this.os_cli_response = process.StandardError.ReadToEnd();
-        process.WaitForExit();
-
-        if (string.IsNullOrEmpty(error)) { return output; }
-        else { return error; }
-    }
-
-    public void run_terminal(string command, string args)
-    {
-        string terminal_application = "";
-        string terminal_arguments = "";
-
-
-        if (OS.Is_windows())
-        {
-            terminal_application = "CMD.exe";
-            terminal_arguments = $@"/C ""{command + " " + args}""";
-        }
-        else
-        {
-            terminal_application = "/bin/bash";
-            terminal_arguments = $@"-c ""{command + " " + args}""";
-        }
-
+        // Debugging
+        // Console.WriteLine($"Running command with args: {command} {args}");
         try
         {
-            var terminal = new Process()
+            var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    FileName = terminal_application,
-                    Arguments = terminal_arguments,
+                    FileName = command,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
                 }
             };
 
-            if(OS.Is_windows())
+            process.Start();
+
+            // Read output and error streams synchronously
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            // Wait for the process to exit
+            process.WaitForExit();
+
+            // Optional timeout handling
+            if (!process.WaitForExit(10000)) // Wait up to 10 seconds
             {
-                terminal.StartInfo.CreateNoWindow = true;
-                terminal.StartInfo.RedirectStandardInput = true;
-                terminal.StartInfo.RedirectStandardOutput = true;
-                terminal.StartInfo.UseShellExecute = false;
+                throw new Exception("The process did not finish within the timeout period.");
             }
 
-            // Mac specific requirements.
-            if (OS.Is_macos())
-            {
-                terminal.StartInfo.RedirectStandardError = false;
-                terminal.StartInfo.UseShellExecute = true;
-            }
+            os_cli_response = error; // Save error response for later access
 
-            terminal.Start();
-            terminal.BeginOutputReadLine();
-            terminal.WaitForExit();
-            terminal.Close();
+            // Return output or error
+            return string.IsNullOrEmpty(error) ? output : error;
         }
         catch (Exception ex)
         {
-            throw ex;
+            throw new Exception($"Command execution failed: {ex.Message}");
         }
+    }
 
+
+    public void run_terminal(string command, string args)
+    {
+        try
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Is_windows() ? "cmd.exe" : "/bin/bash",
+                    Arguments = Is_windows() ? $"/C \"{command} {args}\"" : $"-c \"{command} {args}\"",
+                    RedirectStandardOutput = Is_windows(),
+                    RedirectStandardError = Is_windows(),
+                    UseShellExecute = !Is_windows(),
+                    CreateNoWindow = Is_windows()
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Failed to run terminal command: {ex.Message}");
+        }
     }
 
     public void delete_backup_file(string file_name)
     {
         try
         {
-            foreach (string file in Directory.EnumerateFiles(
-            this.application_data_folder_path,
-            "*",
-            SearchOption.AllDirectories)
-            )
+            foreach (var file in Directory.EnumerateFiles(
+                application_data_folder_path,
+                "*" + file_name + "*",
+                SearchOption.AllDirectories
+            ))
             {
-                if (file is not null && file.EndsWith(file_name))
-                {
-                    File.Delete(file);
-                }
+                File.Delete(file);
             }
         }
-        catch (IOException)
+        catch (IOException ex)
         {
-            throw;
+            throw new Exception($"Failed to delete backup file: {ex.Message}");
         }
     }
 
     public void open_browser(string url)
     {
-        dynamic process;
         try
         {
-            if (OS.Is_windows())
+            if (Is_windows())
             {
-                url = url.Replace("&", "^&");
-                process = Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-                process.Close();
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url.Replace("&", "^&")}") { CreateNoWindow = true });
             }
-            else if (OS.Is_macos())
+            else if (Is_macos())
             {
-                process = Process.Start("open", url);
-                process.Close();
+                Process.Start("open", url);
             }
-            else if (OS.Is_gnu_linux())
+            else if (Is_gnu_linux())
             {
-                process = Process.Start("open", url);
-                process.Close();
+                Process.Start("xdg-open", url);
             }
             else
             {
-                throw new Exception("Looks like I'm not getting paid...");
+                throw new Exception("Unsupported operating system for opening browser.");
             }
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            throw;
+            throw new Exception($"Failed to open browser: {ex.Message}");
         }
-
-        process.Close();
     }
 }
